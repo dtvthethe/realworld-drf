05d2d0901f0958d621fe508b199715fde901673d
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-12-14 23:01:22 +0700

    Add model relations

--- articles/models.py
+++ articles/models.py
@@ -8,6 +8,26 @@ class Article(CoreModel):
     slug = models.CharField(max_length=constants.SLUG_MAX_LENGTH, unique=True)
     description = models.CharField(max_length=constants.DESCRIPTION_MAX_LENGTH)
     body = models.TextField()
+    status = models.SmallIntegerField(default=1)  # 1: Draft, 2: Published
+    author = models.ForeignKey(
+        "users.User",
+        related_name="articles",
+        on_delete=models.CASCADE,
+        db_column="author_id",
+        null=True,
+    )
+    favorites = models.ManyToManyField(
+        "users.User",
+        related_name="favorite_articles",
+        null=True,
+        db_table="favorite_articles",
+    )
+    tags = models.ManyToManyField(
+        "tags.Tag",
+        related_name="article_tags",
+        null=True,
+        db_table="article_tags",
+    )
 
     class Meta:
         db_table = "articles"
