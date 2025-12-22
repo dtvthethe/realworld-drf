5018c2f8ac8c98b17c8ff869d544cea5021b538b
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-12-23 06:24:10 +0700

    register

--- users/api/v1/views.py
+++ users/api/v1/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from users.models import User
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+
+class UserViewSet(GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+
+    def create(self, request):
+        # lấy data từ request
+        # dùng .get() để tránh lỗi "user" không tồn tại
+        user_data = request.data.get("user", {})
+
+        # sử dụng serializer để validate và lưu data
+        serializer = self.get_serializer(data=user_data)
+
+        # nếu data không hợp lệ, raise exception
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        response_data = UserRegisterSerializer(user).data
+
+        return Response({"user": response_data})
