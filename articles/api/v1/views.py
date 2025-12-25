16d69bc3e34f2cfd7fc8cad1342c4b7f23b3f35b
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-12-25 08:26:58 +0700

    delete article

--- articles/api/v1/views.py
+++ articles/api/v1/views.py
@@ -16,7 +16,7 @@ class ArticleViewSet(GenericViewSet):
 
     # nếu chỉ có vài action cần verify token thì override hàm này
     def get_permissions(self):
-        if self.action in ["create"]:
+        if self.action in ["create", "destroy"]:
             return [IsAuthenticated()]
         return []
 
@@ -26,7 +26,9 @@ class ArticleViewSet(GenericViewSet):
             # serializer = self.get_serializer(data=article_data)
             serializer = CreateArticleSerializer(
                 data=article_data,
-                context={"request": request}, # truyền request vào context để sử dụng trong serializer
+                context={
+                    "request": request
+                },  # truyền request vào context để sử dụng trong serializer
             )
             serializer.is_valid(raise_exception=True)
             article = serializer.save()
@@ -44,6 +46,10 @@ class ArticleViewSet(GenericViewSet):
 
             return Response({"article": response_serializer.data}, status=HTTP_200_OK)
             # return Response({"article": {}}, status=HTTP_200_OK)
+        except Article.DoesNotExist:
+            return Response(
+                {"error": "Article not found."}, status=HTTP_400_BAD_REQUEST
+            )
         except Exception as e:
             return Response({"error": e.detail}, status=HTTP_400_BAD_REQUEST)
 
@@ -79,6 +85,28 @@ class ArticleViewSet(GenericViewSet):
                     "articlesCount": articles.count(),
                 },
                 status=HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response({"error": e.detail}, status=HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, slug=None):
+        try:
+            article = self.get_queryset().get(slug=slug)
+            current_user = request.user
+
+            if article.author != current_user:
+                return Response(
+                    {"error": "You are not authorized to delete this article."},
+                    status=HTTP_400_BAD_REQUEST,
+                )
+
+            article.delete()
+            return Response(
+                {"message": "Article deleted successfully."}, status=HTTP_200_OK
+            )
+        except Article.DoesNotExist:
+            return Response(
+                {"error": "Article not found."}, status=HTTP_400_BAD_REQUEST
             )
         except Exception as e:
             return Response({"error": e.detail}, status=HTTP_400_BAD_REQUEST)
