21c0de8b8fae235435c107127074bb67a2b17b14
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-12-26 22:09:00 +0700

    delete comment

--- core/permissions.py
+++ core/permissions.py
@@ -7,6 +7,7 @@ from rest_framework.exceptions import PermissionDenied
 # 2. has_object_permission: check quyền trên object, chỉ hoạt động khi gọi hàm get_object()
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
+        print("111111")
         if obj.author != request.user:
             raise PermissionDenied("You are not the owner of this object.")
         return True
@@ -16,4 +17,17 @@ class IsNotOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         if obj == request.user:
             raise PermissionDenied("You cannot follow/unfollow yourself.")
+        return True
+
+
+class IsCommentOwner(BasePermission):
+    def has_permission(self, request, view):
+        print("222222")
+
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        print(obj.author, request.user)
+        if obj.author != request.user:
+            raise PermissionDenied("You are not the owner of this comment.")
         return True
