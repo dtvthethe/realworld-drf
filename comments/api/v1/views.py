7f9e205f0e4536d334772cb2debfa216e7750aa2
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-12-26 22:27:08 +0700

    delete comment

--- comments/api/v1/views.py
+++ comments/api/v1/views.py
@@ -1,12 +1,13 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.exceptions import APIException
-from rest_framework.permissions import IsAuthenticated
 from comments.api.v1.serializers import (
     CreateCommentSerializer,
     ResponseCommentSerializer,
 )
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from rest_framework.permissions import IsAuthenticated
+from django.http import Http404
 from core.permissions import IsOwner
 from comments.models import Comment
 
@@ -15,8 +16,6 @@ class CommentViewSet(GenericViewSet):
     queryset = Comment.objects.all()
 
     def get_permissions(self):
-        if self.action in ["create"]:
-            return [IsAuthenticated()]
         if self.action in ["destroy"]:
             return [IsAuthenticated(), IsOwner()]
         return []
@@ -62,3 +61,9 @@ class CommentViewSet(GenericViewSet):
             return Response({"comment": response_serializer.data}, status=HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=HTTP_400_BAD_REQUEST)
+        except Http404:
+            return Response({"error": "Comment not found."}, status=HTTP_404_NOT_FOUND)
+        except Comment.DoesNotExist:
+            return Response({"error": "Comment not found."}, status=HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"error": e.detail}, status=HTTP_400_BAD_REQUEST)
