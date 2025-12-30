59eb9e005b6132a52698b1a8aa5d8be522e119ac
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-12-31 00:35:31 +0700

    fix comments

--- comments/api/v1/views.py
+++ comments/api/v1/views.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.exceptions import APIException
 from rest_framework.permissions import IsAuthenticated
-from django.http import Http404
 from core.permissions import IsOwner
 from comments.models import Comment
 
@@ -17,15 +17,20 @@ class CommentViewSet(GenericViewSet):
 
     def destroy(self, request, slug=None, pk=None):
         try:
-            comment = self.get_object()
+            comment = self.get_object()  # get_object() raise Http404 nếu không tìm thấy
             comment.delete()
 
             return Response(
                 {"message": "Comment deleted successfully."}, status=HTTP_200_OK
             )
-        except Http404:
-            return Response({"error": "Comment not found."}, status=HTTP_404_NOT_FOUND)
-        except Comment.DoesNotExist:
-            return Response({"error": "Comment not found."}, status=HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response({"error": e.detail}, status=HTTP_400_BAD_REQUEST)
+            if isinstance(e, APIException):
+                return Response(
+                    {"error": e.detail},
+                    status=e.status_code,
+                )
+
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=HTTP_400_BAD_REQUEST,
+            )
